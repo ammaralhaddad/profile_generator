603f3d325f921ee75f72c08021ba36521cb36ba9
AMMAR AL HADDAD <91353209+ammaralhaddad@users.noreply.github.com>
2021-11-02 19:06:33 -0400

    Create survey.js

---
+++ survey.js
@@ -0,0 +1,53 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question(`What's your name? Nicknames are also acceptable :)`, (answer) => {
+  console.log(`Nice to meet you ${answer}`);
+
+  
+
+  rl.question(`What's an activity you like doing? `, (answer) => {
+    console.log(`you're an active person ${answer}`);
+  
+    rl.question(`What do you listen to while doing that? `, (answer) => {
+      console.log(`thank you for your answer ${answer}`);
+   
+
+      rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
+          console.log(`Thank you for your answer: ${answer}`);
+        
+          rl.question(`What's your favourite thing to eat for that meal? `, (answer) => {
+             console.log(`Thank you for your valuable answer : ${answer}`);
+            
+             rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
+              console.log(`Thank you for your valuable feedback: ${answer}`)
+
+
+
+              l.question('Which sport is your absolute favourite? ', (answer) => {
+              console.log(`Thank you for your valuable answer : ${answer}`);
+
+
+
+              rl.close();
+
+   })
+
+     })
+
+          });
+      })
+     }) })});
+
+
+ 
+
+ 
+
+
+
+
